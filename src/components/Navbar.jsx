776998a540a576b98ccbbfa1c93e7a4449b3e80f
Sinu Kumar <kumarsinu069@gmail.com>
2025-03-10 20:41:16 +0530

    comment  unwantes import

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import HamburgerMenu from "./HamburgerMenu";
+// import HamburgerMenu from "./HamburgerMenu";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
